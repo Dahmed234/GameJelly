e4e3cd6255ef08308b1fde3c16b3e0aec88fafa7
Dahmed234 <115463196+Dahmed234@users.noreply.github.com>
2025-07-29 23:52:48 +0100

    created script

---
+++ Assets/Scripts/GayAssScript.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class GayAssScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log($"fuck you bro");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         Debug.Log(("YAAAAY!"));
+    }
+}
