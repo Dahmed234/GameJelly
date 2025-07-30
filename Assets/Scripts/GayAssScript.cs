0e0d8ac54baea5a86c4f72658970691ef0c4dfc4
Dahmed234 <115463196+Dahmed234@users.noreply.github.com>
2025-07-30 19:09:14 +0100

    begun movement

--- Assets/Scripts/GayAssScript.cs
+++ Assets/Scripts/GayAssScript.cs
@@ -1,17 +1,22 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GayAssScript : MonoBehaviour
 {
+    
+    InputAction moveAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log($"fuck you bro");
+        moveAction = InputSystem.actions.FindAction("move");
     }
 
     // Update is called once per frame
     void Update()
     {
-         Debug.Log(("YAAAAY!"));
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        this.transform.Translate(moveValue.normalized * (5 * Time.deltaTime) , Space.World);
     }
 }
